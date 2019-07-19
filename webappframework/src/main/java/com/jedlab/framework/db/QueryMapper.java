972749455f625e613c5e59c66dd51dcca778cd47
Aydin <paakro@gmail.com>
2019-07-20 01:26:40 +0430

    add in cluse to query mapper

--- webappframework/src/main/java/com/jedlab/framework/db/QueryMapper.java
+++ webappframework/src/main/java/com/jedlab/framework/db/QueryMapper.java
@@ -16,13 +16,21 @@ import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.JoinColumn;
 import javax.persistence.Transient;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.apache.commons.beanutils.PropertyUtils;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Sort;
 
 import com.jedlab.framework.reflections.Property;
 import com.jedlab.framework.reflections.ReflectionUtil;
+import com.jedlab.framework.spring.rest.QueryWhereParser;
+import com.jedlab.framework.spring.rest.QueryWhereParser.FilterProperty;
 import com.jedlab.framework.spring.web.Filter;
 import com.jedlab.framework.spring.web.ParamOperator;
 import com.jedlab.framework.spring.web.ParameterItem;
@@ -205,6 +213,7 @@ public class QueryMapper
         return props;
     }
 
+    @Deprecated
     public static void filterMap(Filter filter, Criteria criteria)
     {
         List<ParameterItem> filterItems = new ArrayList<>();
@@ -285,5 +294,267 @@ public class QueryMapper
             }
         }        
     }
+    
+    
+    /**
+     * build query parameters from sort instance to keep it in pagination
+     * @param sort
+     * @return
+     */
+    public static String sortQueryParams(Sort sort)
+    {
+        if(sort == null)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        sort.forEach(s->{
+            sb.append("&").append("sort=");
+            sb.append(s.getProperty()).append(",").append(s.isAscending() ? "asc" : "desc");
+        });
+        return sb.toString();
+    }
+    
+    /**
+     * build query parameters from filter instance to keep it in pagination
+     * @param filter
+     * @return
+     */
+    public static String queryParams(Filter filter)
+    {
+        Class<? extends Filter> clz = filter.getClass();
+        PropertyDescriptor[] pds = PropertyUtils.getPropertyDescriptors(clz);
+        StringBuilder sb = new StringBuilder();
+        for (PropertyDescriptor pd : pds)
+        {
+            if (pd.getPropertyType().equals(Class.class))
+                continue;
+            try
+            {
+                Method getter = pd.getReadMethod();
+                if (getter != null)
+                {
+                    Object val = pd.getReadMethod().invoke(filter, null);
+                    String propertyName = pd.getName();
+                    if (val == null)
+                        continue;
+                    if (val instanceof String && StringUtil.isEmpty(String.valueOf(val)))
+                    {
+                        continue;
+                    }
+                    sb.append("&");
+                    sb.append(propertyName).append("=").append(val);
+                }
+            }
+            catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if (StringUtil.isEmpty(sb.toString()))
+            return null;
+        return sb.toString();
+    }
+
+    public static List<Predicate> filterMap(List<FilterProperty> filterProperties, CriteriaBuilder cb, CriteriaQuery criteria, Root root, Class<?> clz)
+    {
+        List<Predicate> predicateList = new ArrayList<Predicate>();
+        for (int i = 0; i < filterProperties.size(); i++)
+        {
+            FilterProperty item = filterProperties.get(i);
+            String property = item.getPropertyName();
+            Object value = item.getValue();
+            String operator = item.getOperator();            
+            if(value == null)
+                continue;
+            if(value instanceof String && StringUtil.isEmpty(String.valueOf(value)))
+            {                   
+                continue;
+            }
+            if (property.indexOf(".") > 0)
+            {
+                
+            }
+            else
+            {
+//                ParameterExpression<?> pe = cb.parameter(String.class, property);
+                Field field = ReflectionUtil.getField(clz, property);
+                if (QueryWhereParser.EQ.equals(operator))
+                {
+                    if(field.getType().isEnum())
+                    {
+                        Enum valueOf = Enum.valueOf((Class<Enum>) field.getType(), String.valueOf(value));
+                        predicateList.add(cb.equal(root.get(property), valueOf));
+                    }
+                    else
+                    {
+                        predicateList.add(cb.equal(root.get(property), value));
+                    }
+                }                    
+                else if (QueryWhereParser.NEQ.equals(operator))
+                {
+                    if(field.getType().isEnum())
+                    {
+                        Enum valueOf = Enum.valueOf((Class<Enum>) field.getType(), String.valueOf(value));
+                        predicateList.add(cb.not(cb.equal(root.get(property), valueOf)));
+                    }
+                    else
+                    {
+                        predicateList.add(cb.not(cb.equal(root.get(property), value)));
+                    }
+                }
+                else if (QueryWhereParser.LK.equals(operator))
+                    predicateList.add(cb.like(root.<String> get(property), "%" + value.toString() + "%"));
+                else if (QueryWhereParser.BW.equals(operator))
+                    predicateList.add(cb.like(root.<String> get(property), value.toString() + "%"));
+                else if (QueryWhereParser.EW.equals(operator))
+                    predicateList.add(cb.like(root.<String> get(property), "%" + value.toString()));
+                else if (QueryWhereParser.GT.equals(operator))
+                {
+                    if(Date.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.greaterThan(root.<Date> get(property), new Date(Long.parseLong(v))));
+                    }
+                    if(Number.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.greaterThan(root.<Long> get(property), Long.parseLong(v)));
+                    }                    
+                }
+                else if (QueryWhereParser.GTE.equals(operator))
+                {
+                    if(Date.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.greaterThanOrEqualTo(root.<Date> get(property), new Date(Long.parseLong(v))));
+                    }
+                    if(Number.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.greaterThanOrEqualTo(root.<Long> get(property), Long.parseLong(v)));
+                    }
+                }
+                else if (QueryWhereParser.LT.equals(operator))
+                {
+                    if(Date.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.lessThan(root.<Date> get(property), new Date(Long.parseLong(v))));
+                    }
+                    if(Number.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.lessThan(root.<Long> get(property), Long.parseLong(v)));
+                    }
+                }
+                else if (QueryWhereParser.LTE.equals(operator))
+                {
+                    if(Date.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.lessThanOrEqualTo(root.<Date> get(property), new Date(Long.parseLong(v))));
+                    }
+                    if(Number.class.isAssignableFrom(field.getType()))
+                    {
+                        String v = String.valueOf(value);
+                        predicateList.add(cb.lessThanOrEqualTo(root.<Long> get(property), Long.parseLong(v)));
+                    }
+                }
+                else if(QueryWhereParser.IN.equals(operator) && value instanceof List)
+                {
+                    predicateList.add(cb.in(root.get(property)).value(value));
+                }
+            }
+            
+        }
+       return predicateList;
+    }
+    
+    
+    public static List<Predicate> filterMap(Filter filter, CriteriaBuilder cb, CriteriaQuery criteria, Root root)
+    {
+        List<ParameterItem> filterItems = new ArrayList<>();
+        Class<? extends Filter> clz = filter.getClass();
+        PropertyDescriptor[] pds = PropertyUtils.getPropertyDescriptors(clz);
+        for (PropertyDescriptor pd : pds)
+        {
+            if (pd.getPropertyType().equals(Class.class))
+                continue;
+            try
+            {
+                Method getter = pd.getReadMethod();
+                if (getter != null && getter.isAnnotationPresent(QParam.class))
+                {
+                    QParam annotation = getter.getAnnotation(QParam.class);
+                    Object invoke = pd.getReadMethod().invoke(filter, null);
+                    String propertyName = pd.getName();
+                    if(StringUtil.isNotEmpty(annotation.propertyName()))
+                        propertyName = annotation.propertyName();
+                    filterItems.add(new ParameterItem(propertyName, annotation.operator(), invoke));
+                }
+            }
+            catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        //
+        List<Predicate> predicateList = new ArrayList<Predicate>();
+        for (ParameterItem item : filterItems)
+        {
+            Object val = item.getValue();
+            if(val == null)
+                continue;
+            if(val instanceof String && StringUtil.isEmpty(String.valueOf(val)))
+            {                   
+                continue;
+            }
+            if(val instanceof Date)
+            {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime((Date)val);
+                cal.set(Calendar.HOUR, 0);
+                cal.set(Calendar.MINUTE, 0);
+                cal.set(Calendar.SECOND, 0);
+                cal.set(Calendar.MILLISECOND, 0);
+                val = cal.getTime();
+            }
+            if (ParamOperator.LIKE.equals(item.getOperator()))
+            {
+                predicateList.add(cb.like(root.<String> get(item.getPropertyName()), "%" + val.toString() + "%"));
+            }
+            if (ParamOperator.EQ.equals(item.getOperator()))
+            {
+                predicateList.add(cb.equal(root.get(item.getPropertyName()), val));
+            }
+            if (ParamOperator.NEQ.equals(item.getOperator()))
+            {
+                predicateList.add(cb.not(cb.equal(root.get(item.getPropertyName()), val)));
+            }
+            if (ParamOperator.GT.equals(item.getOperator()))
+            {
+                predicateList.add(cb.greaterThan(root.<Long> get(item.getPropertyName()), (Long)val));
+            }
+            if (ParamOperator.LT.equals(item.getOperator()))
+            {
+                predicateList.add(cb.lessThan(root.<Long> get(item.getPropertyName()), (Long)val));
+            }
+            if (ParamOperator.LTE.equals(item.getOperator()))
+            {
+                predicateList.add(cb.lessThanOrEqualTo(root.<Long>get(item.getPropertyName()), (Long)val));
+            }
+            if (ParamOperator.GTE.equals(item.getOperator()))
+            {
+                predicateList.add(cb.greaterThanOrEqualTo(root.<Long>get(item.getPropertyName()), (Long)val));
+            }
+            if (ParamOperator.SQLQUERY.equals(item.getOperator()))
+            {
+                throw new UnsupportedOperationException("not supported in JPA");
+//                criteria.add(Restrictions.sqlRestriction(String.valueOf(item.getValue())));
+            }
+        }        
+        return predicateList;
+    }
+    
+    
 
 }
